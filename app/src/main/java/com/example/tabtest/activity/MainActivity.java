04d4d8f4cc7d5b39fdd32a4ae7edb7dd269d29a2
Yevhen Yefimov <yefimovyevhen@gmail.com>
2021-03-02 10:31:55 +0300

    first commit

--- app/src/main/java/com/example/tabtest/activity/MainActivity.java
+++ app/src/main/java/com/example/tabtest/activity/MainActivity.java
@@ -1,38 +1,89 @@
 package com.example.tabtest.activity;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.tabtest.R;
 import com.example.tabtest.SectionsPagerAdapter;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.material.navigation.NavigationView;
 import com.google.android.material.tabs.TabLayout;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 import androidx.viewpager.widget.ViewPager;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+import java.util.concurrent.atomic.AtomicReference;
+
 public class MainActivity extends AppCompatActivity {
+    private final int LOCATION_PERMISSION_CODE = 101;
     public static ViewPager viewPager;
     private SectionsPagerAdapter sectionsPagerAdapter;
     private ActionBarDrawerToggle toggle;
     private TabLayout tabs;
     private DrawerLayout drawerLayout;
     private NavigationView navigationView;
-
+    private SwipeRefreshLayout refreshLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        setSupportActionBar(findViewById(R.id.toolbar_mainActivity));
 
-        // init tabs
+        initRefreshLayout();
+        initTabs();
+        initNavView();
+
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(ScreenActivity.CITY_MESSAGE);
+        if (message != null) {
+            getSupportActionBar().setTitle(message);
+        } else {
+            getSupportActionBar().setTitle("default");
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (toggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void initRefreshLayout() {
+        refreshLayout = findViewById(R.id.swipeRefreshLayout_mainActivity);
+        refreshLayout.setOnRefreshListener(() -> {
+            if (checkLocationPermissions()) {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION,
+                        Manifest.permission.ACCESS_COARSE_LOCATION}, LOCATION_PERMISSION_CODE);
+            }
+            setCity();
+            Toast.makeText(this, "REFRESH", Toast.LENGTH_SHORT).show();
+        });
+    }
+
+    private void initTabs() {
         sectionsPagerAdapter = new SectionsPagerAdapter(this,
                 getSupportFragmentManager(),
                 FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
@@ -40,18 +91,14 @@ public class MainActivity extends AppCompatActivity {
         viewPager.setAdapter(sectionsPagerAdapter);
         tabs = findViewById(R.id.tabLayout_mainActivity);
         tabs.setupWithViewPager(viewPager);
+    }
 
-        // init actionBar
-        setSupportActionBar(findViewById(R.id.toolbar_mainActivity));
-
+    private void initNavView() {
         drawerLayout = findViewById(R.id.draverLayout_mainActivity_mainContainer);
-
         toggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close);
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
-
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
         navigationView = findViewById(R.id.navView_mainActivity);
         navigationView.setNavigationItemSelectedListener((item) -> {
             switch (item.getItemId()) {
@@ -70,15 +117,40 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        if (toggle.onOptionsItemSelected(item)) {
-            return true;
+    private void setCity() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
         }
-        return super.onOptionsItemSelected(item);
+        FusedLocationProviderClient fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        fusedLocationClient.getLastLocation().addOnSuccessListener((location) -> {
+            if (location != null) {
+                try {
+                    Geocoder geocoder = new Geocoder(getApplicationContext(), Locale.getDefault());
+                    List<Address> addresses = geocoder.
+                            getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+                    getSupportActionBar().setTitle(addresses.get(0).getLocality());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 
-    public SectionsPagerAdapter getSectionsPagerAdapter() {
-        return sectionsPagerAdapter;
+    private boolean checkLocationPermissions() {
+        return ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(getApplicationContext(),
+                android.Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == LOCATION_PERMISSION_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                setCity();
+            }
+        }
     }
 }
--- app/src/main/java/com/example/tabtest/activity/ScreenActivity.java
+++ app/src/main/java/com/example/tabtest/activity/ScreenActivity.java
@@ -1,55 +1,90 @@
 package com.example.tabtest.activity;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.os.Bundle;
-import android.view.Window;
 import android.view.WindowManager;
-import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import com.example.tabtest.R;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.tasks.Task;
 
-import static android.view.View.SYSTEM_UI_FLAG_FULLSCREEN;
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class ScreenActivity extends AppCompatActivity {
-    private FusedLocationProviderClient fusedLocationClient;
+    public static final String CITY_MESSAGE = "com.example.tabtest.activity.CITY_MESSAGE";
+    private final int LOCATION_PERMISSION_CODE = 101;
+    private String result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_screen);
-        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
-        fetchLocation();
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(getApplicationContext(),
+                android.Manifest.permission.ACCESS_COARSE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            findCity();
+            System.out.println(result);
+            startMainActivity();
+        } else {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION}, LOCATION_PERMISSION_CODE);
+        }
+    }
 
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == LOCATION_PERMISSION_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                findCity();
+            }
+            startMainActivity();
+        }
+    }
+
+    void startMainActivity() {
+        Intent intent = new Intent(this, MainActivity.class);
+        if (result != null) {
+            intent.putExtra(ScreenActivity.CITY_MESSAGE, result);
+        }
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
 
     }
 
-    private void fetchLocation() {
-
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED
-                &&
-                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
-                        != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(
-                    this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 101);
-            return;
+    private void findCity() {
+        FusedLocationProviderClient fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
         }
-        Task<Location> task = fusedLocationClient.getLastLocation();
-        task.addOnSuccessListener((it) -> {
-            if (it != null) {
-                System.out.println(it.getLongitude() + " " + it.getLatitude());
+        fusedLocationClient.getLastLocation().addOnSuccessListener(this, location -> {
+            if (location != null) {
+                try {
+                    Geocoder geocoder = new Geocoder(getApplicationContext(), Locale.getDefault());
+                    List<Address> addresses = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+                    //res.compareAndSet(addresses.get(0).getLocality(), addresses.get(0).getLocality());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
         });
     }
-
-
 }
